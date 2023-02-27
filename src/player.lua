ad083805d9c6c41c7c39abdb153729aeaa7d37ec
LuccaC.M <git.yfm@slmail.me>
2023-02-27 17:49:18 -0500

    Add Shooting functionality

--- src/player.lua
+++ src/player.lua
@@ -5,6 +5,9 @@ PlayerModule.MetaPlayer.__index = PlayerModule.Player
 
 PlayerModule.Player.y = 400
 PlayerModule.Player.x = love.graphics.getWidth() / 2
+PlayerModule.Player.shotX = 50
+PlayerModule.Player.shotY = 50
+PlayerModule.Player.shooting = false
 
 function PlayerModule.Player:new(name)
     local instance = setmetatable({}, PlayerModule.MetaPlayer)
@@ -12,7 +15,15 @@ function PlayerModule.Player:new(name)
     return instance
 end
 
-function PlayerModule.Player:MovePlayer(steps, deltaTime)
+-- Position the shoot inside the player to be "invisible"
+function PlayerModule.Player:PosShoot()
+    if (not self.shooting) then
+        self.shotX = self.x + 20
+        self.shotY = self.y
+    end
+end
+
+function PlayerModule.Player:MovePlayer(steps,deltaTime)
     if love.keyboard.isDown('d') then
         self.x = self.x + steps * deltaTime;
     end
@@ -21,9 +32,19 @@ function PlayerModule.Player:MovePlayer(steps, deltaTime)
     end
 end
 
-function PlayerModule.Player:Shoot()
+function PlayerModule.Player:Shoot(deltaTime)
+--  if space if is pressed then start shooting  
     if love.keyboard.isDown('space') then
-        print("Shoot")
+        self.shooting = true
+    end
+--  if the shot is outside the screen change to not shooting state
+    if (self.shotY < -10) then
+        self.shooting = false
+    end
+--  move the shot if it is in the shooting state
+    if(self.shooting) then
+        self.shotY = self.shotY - 200 * deltaTime
     end
 end
+
 return PlayerModule
