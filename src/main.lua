bae772b7967b51539adb154cdcfa2486d11f3dc6
LuccaC.M <git.yfm@slmail.me>
2023-03-11 12:56:56 -0500

    Add Enemy Array

--- src/main.lua
+++ src/main.lua
@@ -4,7 +4,8 @@ PlayerModule = require("player")
 function love.load()
 --  Initialize the Player & Enemy
     Player = PlayerModule.Player:new()
-    EnemyOne = EnemyModule.Enemy:new()
+    Enemies = {EnemyModule.Enemy:new(), EnemyModule.Enemy:new()}
+--    Enemies[1] = EnemyModule.Enemy:new()
 --  Global Variables
     PlayerHasLost = false
 end
@@ -13,18 +14,19 @@ function love.update(dt)
     Player:MovePlayer(dt)
     Player:PosShoot()
     Player:Shoot(dt)
-    if (EnemyOne.alive) then
-        EnemyOne:CheckEnemy()
-        EnemyOne:MoveEnemy(dt)
+    if (Enemies[1].alive) then
+        Enemies[1]:CheckEnemy()
+        Enemies[1]:MoveEnemy(dt)
     end
-    if (DetectCollision(-10, 1100, 455, 150, EnemyOne.x, 50, EnemyOne.y, 50)) then
+--  if the enemy collides with the Floor the Player looses
+    if (DetectCollision(-10, 1100, 455, 150, Enemies[1].x, 50, Enemies[1].y, 50)) then
         PlayerHasLost = true
     end
 --  if the shot collides with the Enemy then kill the Enemy
-    if (DetectCollision(Player.shotX, 15, Player.shotY, 25, EnemyOne.x, 50, EnemyOne.y, 50)) then
-        EnemyOne.alive = false
-        EnemyOne.x = 10000
-        EnemyOne.y = 10000
+    if (DetectCollision(Player.shotX, 15, Player.shotY, 25, Enemies[1].x, 50, Enemies[1].y, 50)) then
+        Enemies[1].alive = false
+        Enemies[1].x = 10000
+        Enemies[1].y = 10000
         Player.shotY = 10000
         Player.shooting = false
     end
@@ -36,8 +38,8 @@ function love.draw()
 --  Shot
     love.graphics.rectangle("fill", Player.shotX, Player.shotY, 15, 25)
 --  Enemy
-    if (EnemyOne.alive) then
-        love.graphics.rectangle("line", EnemyOne.x, EnemyOne.y, 50, 50)
+    if (Enemies[1].alive) then
+        love.graphics.rectangle("line", Enemies[1].x, Enemies[1].y, 50, 50)
     end
 --  Floor
     love.graphics.rectangle("line", -10, 455, 1100, 150)
