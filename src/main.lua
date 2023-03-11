8e13228c1b7172f227c2934b3d12b220d7bb69cb
LuccaC.M <git.yfm@slmail.me>
2023-03-11 13:25:33 -0500

    Add Second Enemy

--- src/main.lua
+++ src/main.lua
@@ -14,22 +14,8 @@ function love.update(dt)
     Player:MovePlayer(dt)
     Player:PosShoot()
     Player:Shoot(dt)
-    if (Enemies[1].alive) then
-        Enemies[1]:CheckEnemy()
-        Enemies[1]:MoveEnemy(dt)
-    end
---  if the enemy collides with the Floor the Player looses
-    if (DetectCollision(-10, 1100, 455, 150, Enemies[1].x, 50, Enemies[1].y, 50)) then
-        PlayerHasLost = true
-    end
---  if the shot collides with the Enemy then kill the Enemy
-    if (DetectCollision(Player.shotX, 15, Player.shotY, 25, Enemies[1].x, 50, Enemies[1].y, 50)) then
-        Enemies[1].alive = false
-        Enemies[1].x = 10000
-        Enemies[1].y = 10000
-        Player.shotY = 10000
-        Player.shooting = false
-    end
+    Enemies[1]:Attack(dt, Player)
+    Enemies[2]:Attack(dt, Player)
 end
 
 function love.draw()
@@ -37,9 +23,12 @@ function love.draw()
     love.graphics.rectangle("fill", Player.x, Player.y, 50, 50)
 --  Shot
     love.graphics.rectangle("fill", Player.shotX, Player.shotY, 15, 25)
---  Enemy
+--  Enemies
     if (Enemies[1].alive) then
         love.graphics.rectangle("line", Enemies[1].x, Enemies[1].y, 50, 50)
+    end
+    if (Enemies[2].alive) then
+        love.graphics.rectangle("line", Enemies[2].x, Enemies[2].y, 50, 50)
     end
 --  Floor
     love.graphics.rectangle("line", -10, 455, 1100, 150)
